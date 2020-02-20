5c5044cb1ff5fcf6b74862b0038a9598a5fe1cce
Mat <mnantel@gmail.com>
2020-02-20 07:18:32 -0500

    add push to FSA

--- viriius.go
+++ viriius.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"bytes"
 	"crypto/tls"
+	"encoding/base64"
+	"encoding/json"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"time"
@@ -11,13 +15,6 @@ import (
 	"github.com/MonaxGT/gomalshare"
 	bolt "go.etcd.io/bbolt"
 )
-
-// const (
-// 	malshareAPIKey = "1d3e5832208312e0955491bc25b9fd5aff9e2d916e3dae4ba2d212b9f29e4e6d"
-// 	path           = "viriius.db"
-// )
-
-// go run viriius.go -a 1d3e5832208312e0955491bc25b9fd5aff9e2d916e3dae4ba2d212b9f29e4e6d -p viriius.db -d
 
 var (
 	db             *bolt.DB
@@ -27,6 +24,8 @@ var (
 	ignore         = flag.Bool("i", false, "Ignore existing hash list and download all new files.")
 	ssl            = flag.Bool("s", false, "Disable SSL certificate validation (useful when testing inline MITM inspection device).")
 	exists         = flag.Bool("e", false, "Output message for files that already exist.")
+	fsaKey         string
+	fsaSession     string
 )
 
 var (
@@ -46,6 +45,57 @@ var (
 	White   = Color("\033[1;37m%s\033[0m")
 )
 
+type apiFSAUpload struct {
+	Method  string               `json:"method"`
+	Params  []apiFSAUploadParams `json:"params"`
+	Session string               `json:"session"`
+	ID      string               `json:"id"`
+	Ver     string               `json:"ver"`
+}
+type apiFSAUploadMeta struct {
+	MetaFilename string `json:"meta_filename"`
+	MetaURL      string `json:"meta_url"`
+}
+type apiFSAUploadParams struct {
+	File            string           `json:"file"`
+	Filename        string           `json:"filename"`
+	SkipSteps       string           `json:"skip_steps"`
+	URL             string           `json:"url"`
+	Type            string           `json:"type"`
+	OverwriteVMList string           `json:"overwrite_vm_list"`
+	ArchivePassword string           `json:"archive_password"`
+	Meta            apiFSAUploadMeta `json:"meta"`
+	Timeout         string           `json:"timeout"`
+}
+
+type apiFSALogin struct {
+	Method string              `json:"method"`
+	Params []apiFSALoginParams `json:"params"`
+	ID     string              `json:"id"`
+	Ver    string              `json:"ver"`
+}
+type apiFSALoginData struct {
+	User   string `json:"user"`
+	Passwd string `json:"passwd"`
+}
+type apiFSALoginParams struct {
+	URL  string            `json:"url"`
+	Data []apiFSALoginData `json:"data"`
+}
+
+type apiFSALoginResponse struct {
+	ID     int    `json:"id"`
+	Ver    string `json:"ver"`
+	Result struct {
+		URL    string `json:"url"`
+		Status struct {
+			Code    int    `json:"code"`
+			Message string `json:"message"`
+		} `json:"status"`
+	} `json:"result"`
+	Session string `json:"session"`
+}
+
 func Color(colorString string) func(...interface{}) string {
 	sprint := func(args ...interface{}) string {
 		return fmt.Sprintf(colorString,
@@ -56,6 +106,7 @@ func Color(colorString string) func(...interface{}) string {
 
 func main() {
 
+	loginFSA()
 	flag.Parse()
 
 	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
@@ -86,13 +137,14 @@ func main() {
 		fmt.Printf("Sample %d MD5: %s", i, e.Md5)
 		if !existHash(e.Md5) || *ignore {
 			if !*dryrun {
-				_, err := conf.DownloadFileFromHash(e.Md5)
+				file, err := conf.DownloadFileFromHash(e.Md5)
 				if err != nil {
 					fmt.Printf("...%s %s\r\n", Red("ERROR"), Red(err))
 					continue
 				}
 				addHash(e.Md5)
 				fmt.Printf("...%s\r\n", Green("DOWNLOADED"))
+				submitFileFSA(file, e)
 				continue
 			}
 			fmt.Printf("...%s\r\n", Yellow("SKIP (DRYRUN)"))
@@ -132,4 +184,87 @@ func existHash(hash string) bool {
 		return true
 	}
 	return false
+}
+
+func loginFSA() {
+
+	fsaLoginData := apiFSALoginData{
+		User:   "admin",
+		Passwd: "FortiQc2012",
+	}
+	fsaLoginParams := apiFSALoginParams{
+		URL: "/sys/login/user",
+	}
+	fsaLogin := apiFSALogin{
+		Method: "exec",
+		ID:     "1",
+		Ver:    "2.0",
+	}
+	fsaLoginParams.Data = append(fsaLoginParams.Data, fsaLoginData)
+	fsaLogin.Params = append(fsaLogin.Params, fsaLoginParams)
+
+	login, err := json.Marshal(fsaLogin)
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	client := &http.Client{Transport: tr}
+	reqURI := fmt.Sprintf("https://%s:%s/%s", "192.168.129.15", "443", "jsonrpc")
+	req, err := http.NewRequest("POST", reqURI, bytes.NewBuffer(login))
+	if err != nil {
+		fmt.Println(err)
+	}
+	res, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	resbody, _ := ioutil.ReadAll(res.Body)
+	var response apiFSALoginResponse
+	_ = json.Unmarshal(resbody, &response)
+	fsaSession = response.Session
+	fmt.Println(fsaSession)
+}
+
+func submitFileFSA(file []byte, meta gomalshare.HashList) bool {
+
+	// enc := base64.StdEncoding.EncodeToString(file)
+
+	uploadParams := apiFSAUploadParams{
+		File:      base64.StdEncoding.EncodeToString(file),
+		Filename:  base64.StdEncoding.EncodeToString([]byte(meta.Md5)),
+		SkipSteps: "1,2,8",
+		URL:       "/alert/ondemand/submit-file",
+		Type:      "file",
+	}
+
+	uploadFSA := apiFSAUpload{
+		Method:  "set",
+		Session: fsaSession,
+		ID:      "1",
+		Ver:     "2.5",
+	}
+
+	uploadFSA.Params = append(uploadFSA.Params, uploadParams)
+	upload, err := json.Marshal(uploadFSA)
+
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	client := &http.Client{Transport: tr}
+	reqURI := fmt.Sprintf("https://%s:%s/%s", "192.168.129.15", "443", "jsonrpc")
+	req, err := http.NewRequest("POST", reqURI, bytes.NewBuffer(upload))
+	if err != nil {
+		fmt.Println(err)
+	}
+	res, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	resbody, _ := ioutil.ReadAll(res.Body)
+	fmt.Println(string(resbody))
+	// var response apiFSALoginResponse
+	// _ = json.Unmarshal(resbody, &response)
+	// fsaSession = response.Session
+	// fmt.Println(fsaSession)
+
+	return true
 }
